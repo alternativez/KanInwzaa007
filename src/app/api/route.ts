84962191b186808aab16b6f373bc3d3576311441
Terdtham R <terdthamz@gmail.com>
2025-07-09 19:33:43 +0700

    Get Post Ready

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,7 +1,34 @@
+let PersonalData = [
+  {
+    id: 1,
+    firstname: 'John',
+    lastname: 'Doe',
+    year: 1,
+    allergy: 'None',
+    birthdate: '2000-01-01',
+    description: 'A brief description about John Doe.',
+  },
+];
+
+let counter = 2;
+
 export async function GET() {
-  return Response.json({ name: 'John Doe', age: 30, city: 'New York' })
+  return Response.json({ PersonalData });
 }
 
-export async function POST() {
-  return Response.json({ message: 'Data received successfully' })
+export async function POST(request: Request) {
+  const data = await request.json();
+
+  const NewPersonalData = {
+    id: counter++,
+    firstname: data.firstname,
+    lastname: data.lastname,
+    year: data.year,
+    allergy: data.allergy || 'None.',
+    birthdate: data.birthdate,
+    description: data.description || 'No description provided.',
+  };
+
+  PersonalData.push(NewPersonalData);
+  return Response.json({ message: 'Data received successfully', newEntry: NewPersonalData });
 }
